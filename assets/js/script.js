40df502349d7bb435f04c532228030b86b8ac1a9
AleciaMPerez <alecia.perez@hotmail.com>
2023-05-18 09:02:02 -0400

    Office hours: added parseint

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 // HOW DO I MAKE THE TIME CONTINUOUS?
 var todayDate = dayjs().format("dddd, MMM DD, YYYY, h:mm A");
 $("#currentDay").text(todayDate);
@@ -6,43 +5,41 @@ $("#currentDay").text(todayDate);
 // When the save button is selected
 $(document).ready(function () {
   $(".saveBtn").on("click", function () {
-       var text = $(this).siblings(".description").val();
-       var time = $(this).parent().attr("id");
-  console.log(text)
-  console.log(time)
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    console.log(text);
+    console.log(time);
 
-// Save to the local storage
-localStorage.setItem(text, time);
-})
-})
+    // Save to the local storage
+    localStorage.setItem(text, time);
+  });
+});
 
 // pull from the local storage
 
-// Remove entry if needed
-
 // The time blocks are highlighted to indicate the past, present, future
+// for each time-block class, run the following function
+$(".time-block").each(function () {
+  
+  var time = parseInt($(this).attr("id").split("-")[1]);
+  console.log(time);
 
-// for each time-block class, run the below function
-$(".time-block").each(function(){
- var time = $(this).attr("id")
- console.log(time)
- //why do I need to split the time? How do I do this?
- var currentHour = dayjs().hour()
-console.log(currentHour)
-// if the time is less than current time, it is past
-if (time < currentHour) {
-  $(this).addClass("past");
-// if time is equal to current time, it is present
-} else if (time === currentHour){
-  $(this).removeClass("past")
-  $(this).addClass("present")
-  // if time is more than current time, it is future
-}else {
-  $(this).removeClass("present");
-  $(this).removeClass("past")
-   $(this).addClass("future");
-}
- 
-}
-
-)
+  var currentHour = dayjs().hour();
+  console.log(currentHour);
+  // if the time is less than current time, it is past
+  if (time < currentHour) {
+    $(this).addClass("past");
+    $(this).removeClass("present");
+    $(time).removeClass("future");
+    // if time is equal to current time, it is present
+  } else if (time === currentHour) {
+    $(this).removeClass("past");
+    $(this).addClass("present");
+    $(this).removeClass("future");
+    // if time is more than current time, it is future
+  } else {
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future");
+  }
+});
